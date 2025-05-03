1d944ba6644e1c31700013cf2d667bf2c8ae19ff
Vipul Tyagi <tyagivipul11.vt@gmail.com>
2025-05-04 00:03:52 +0200

    RAPTOR original model class files

---
+++ 00_Basework_Reproduce/00_raptor/src/components/QAModels.py
@@ -0,0 +1,148 @@
+import logging
+import os
+
+from openai import OpenAI
+
+
+import getpass
+from abc import ABC, abstractmethod
+
+import torch
+from tenacity import retry, stop_after_attempt, wait_random_exponential
+from transformers import T5ForConditionalGeneration, T5Tokenizer
+from dotenv import load_dotenv
+
+load_dotenv()
+
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+
+
+class BaseQAModel(ABC):
+    @abstractmethod
+    def answer_question(self, context, question):
+        pass
+
+
+class GPT4ominiQAModel(BaseQAModel):
+    def __init__(self, model="gpt-4o-mini"):
+        """
+        Initializes the GPT-4o-mini model with the specified model version.
+
+        Args:
+            model (str, optional): The GPT-4o-mini model version to use for generating summaries. Defaults to "gpt-4o-mini".
+        """
+        self.model = model
+        self.client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+
+    @retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(6))
+    def _attempt_answer_question(
+        self, context, question, max_tokens=150, stop_sequence=None
+    ):
+        """
+        Generates a summary of the given context using the GPT-4o-mini model.
+
+        Args:
+            context (str): The text to summarize.
+            max_tokens (int, optional): The maximum number of tokens in the generated summary. Defaults to 150.
+            stop_sequence (str, optional): The sequence at which to stop summarization. Defaults to None.
+
+        Returns:
+            str: The generated summary.
+        """
+        response = self.client.chat.completions.create(
+            model=self.model,
+            messages=[
+                {"role": "system", "content": "You are Question Answering Portal"},
+                {
+                    "role": "user",
+                    "content": f"Given Context: {context} Give the best full answer amongst the option to question {question}",
+                },
+            ],
+            temperature=0,
+        )
+
+        return response.choices[0].message.content.strip()
+
+    @retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(6))
+    def answer_question(self, context, question, max_tokens=150, stop_sequence=None):
+
+        try:
+            return self._attempt_answer_question(
+                context, question, max_tokens=max_tokens, stop_sequence=stop_sequence
+            )
+        except Exception as e:
+            print(e)
+            return e
+
+
+class GPT4oQAModel(BaseQAModel):
+    def __init__(self, model="gpt-4o"):
+        """
+        Initializes the GPT-4o model with the specified model version.
+
+        Args:
+            model (str, optional): The GPT-4o-mini model version to use for generating summaries. Defaults to "gpt-4o-mini".
+        """
+        self.model = model
+        self.client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+
+    @retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(6))
+    def _attempt_answer_question(
+        self, context, question, max_tokens=150, stop_sequence=None
+    ):
+        """
+        Generates a summary of the given context using the GPT-4o-mini model.
+
+        Args:
+            context (str): The text to summarize.
+            max_tokens (int, optional): The maximum number of tokens in the generated summary. Defaults to 150.
+            stop_sequence (str, optional): The sequence at which to stop summarization. Defaults to None.
+
+        Returns:
+            str: The generated summary.
+        """
+        response = self.client.chat.completions.create(
+            model=self.model,
+            messages=[
+                {"role": "system", "content": "You are Question Answering Portal"},
+                {
+                    "role": "user",
+                    "content": f"Given Context: {context} Give the best full answer amongst the option to question {question}",
+                },
+            ],
+            temperature=0,
+        )
+
+        return response.choices[0].message.content.strip()
+
+    @retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(6))
+    def answer_question(self, context, question, max_tokens=150, stop_sequence=None):
+
+        try:
+            return self._attempt_answer_question(
+                context, question, max_tokens=max_tokens, stop_sequence=stop_sequence
+            )
+        except Exception as e:
+            print(e)
+            return e
+
+
+class UnifiedQAModel(BaseQAModel):
+    def __init__(self, model_name="allenai/unifiedqa-v2-t5-3b-1363200"):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = T5ForConditionalGeneration.from_pretrained(model_name).to(
+            self.device
+        )
+        self.tokenizer = T5Tokenizer.from_pretrained(model_name)
+
+    def run_model(self, input_string, **generator_args):
+        input_ids = self.tokenizer.encode(input_string, return_tensors="pt").to(
+            self.device
+        )
+        res = self.model.generate(input_ids, **generator_args)
+        return self.tokenizer.batch_decode(res, skip_special_tokens=True)
+
+    def answer_question(self, context, question):
+        input_string = question + " \\n " + context
+        output = self.run_model(input_string)
+        return output[0]
